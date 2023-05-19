0fa8e57c50788528911ac72e981db3eb218bd897
QaeyxL <132969906+QaeyxL@users.noreply.github.com>
2023-05-19 09:44:58 +0800

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+function clock() {
+  var time = new Date(),
+    hours = time.getHours(),
+    minutes = time.getMinutes();
+
+  document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes);
+
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn;
+    }
+    return standIn;
+  }
+}
+
+setInterval(clock, 1000);
